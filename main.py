13ffbda5817ed5f8adab83304d1755cc5c1c1bbb
ismailmalygin4-hue <ismailmalygin4@gmail.com>
2026-08-31 22:28:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# Вставьте ваш новый токен из BotFather прямо в кавычки ниже:
 BOT_TOKEN = "8818268231:AAGbMU8_r40HRmB5773kS9dGK2e1yjR4h1g"
 REF_LINK_1WIN = "https://one-vv4866.com/open-register&p=i398"
 ADMIN_USERNAME = "@Dexterslive"
