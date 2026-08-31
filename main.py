d83b6a148c7e38b754cb5d7af3d28e29d8b1aa15
ismailmalygin4-hue <ismailmalygin4@gmail.com>
2026-08-31 22:07:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        print("Бот остановлен.")                                                               
+        print("Бот остановлен.")
