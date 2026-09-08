6060ebf6937b089568f40de96aa36ce1de1d3e25
ismailmalygin4-hue <ismailmalygin4@gmail.com>
2026-09-08 17:58:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.filters import CommandStart
 logging.basicConfig(level=logging.INFO)
 
 # Получаем токен из переменных окружения Render
-TOKEN = os.getenv("BOT_TOKEN", "8818268231:AAF26_KJH8qlLeUgJ3uX4Jp71xQFMyKGe6s")
+TOKEN = os.getenv("BOT_TOKEN", "8818268231:AAFc4GG0LImEyzitYYIr6pQ-j7JxGH05UFA")
 PORT = int(os.getenv("PORT", 10000))
 
 # Ссылка на твое веб-приложение на Render
